4ea15e3bebdd48eb3b325ee0253f5cc6be88cc42
dany-eduard <danyeduard17@gmail.com>
2021-05-29 22:55:49 -0500

    Update: 4. Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,41 @@
 #!/usr/bin/python3
-""" Create API  with Flask """
-from flask import Flask, jsonify
-from models import storage
-from models.engine import *
+""" Flask Application """
 from api.v1.views import app_views
-from os import getenv
+from flasgger import Swagger
+from flasgger.utils import swag_from
+from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
+from models import storage
+from os import environ
 
-""" Instance of Flask """
 app = Flask(__name__)
-
-""" Register the blueprint app_views """
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_storage(error):
-    """ Close the db """
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """ Handler for 404 errors that returns a JSON """
-    return (jsonify(error="Not found"), 404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
@@ -35,4 +46,4 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if not port:
         port = '5000'
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
